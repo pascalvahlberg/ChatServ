dd12b50fabcb910fffe566745e1cb97a37032672
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-18 13:27:15 +0100

    checksum: 218edea5ec3a9d478b84abc3602ec7c7
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- server/server.vb
+++ server/server.vb
@@ -17,6 +17,7 @@ Module server
     Private config_ip As String = conf.load("network", "ip") 'currently buggy when other ip than 0.0.0.0
     Private config_port As String = conf.load("network", "port")
     Private config_name As String = conf.load("network", "name")
+    Private config_host As String = conf.load("network", "host")
     Private ipendpoint As IPEndPoint = New IPEndPoint(IPAddress.Parse(config_ip), config_port)
     Private list As New List(Of Connection)
     Private users As New List(Of String)
@@ -108,28 +109,29 @@ Module server
     Private Sub RegisterServer()
         Dim server As New TcpClient
         Try
-            server.Connect("chiruclan.de", 8001)
-            If server.Connected Then
-                Console.WriteLine("*** Registered Server")
-                Dim c As New Connection
-                c.stream = server.GetStream
-                c.streamr = New StreamReader(c.stream)
-                c.streamw = New StreamWriter(c.stream)
-                c.streamw.WriteLine(config_name & " " & config_port)
-                c.streamw.Flush()
-                While server.Connected
-                    c.streamr.ReadLine()
-                End While
-                c.stream.Close()
-                c.streamw.Close()
-                c.streamr.Close()
-                Console.WriteLine("*** Unregistered Server")
+            If Not String.IsNullOrWhiteSpace(config_host) Then
+                server.Connect("localhost", 8001)
+                If server.Connected Then
+                    Console.WriteLine("*** Registered Server")
+                    Dim c As New Connection
+                    c.stream = server.GetStream
+                    c.streamr = New StreamReader(c.stream)
+                    c.streamw = New StreamWriter(c.stream)
+                    c.streamw.WriteLine(config_name & " " & config_host & " " & config_port)
+                    c.streamw.Flush()
+                    While server.Connected
+                        c.streamr.ReadLine()
+                    End While
+                    c.stream.Close()
+                    c.streamw.Close()
+                    c.streamr.Close()
+                    Console.WriteLine("*** Unregistered Server")
+                End If
+                server.Close()
+                RegisterServer()
             End If
-            server.Close()
-            Dim t As New Threading.Thread(AddressOf RegisterServer)
-            t.Start()
         Catch ex As Exception
-
+            RegisterServer()
         End Try
     End Sub
 
